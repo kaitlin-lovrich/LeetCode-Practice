40247bcfd53284f78628d9cfe29d0633e87c6456
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-01-30 12:21:11 -0800

    Completed Fizz-Buzz

---
+++ Fizz-Buzz/main.js
@@ -0,0 +1,21 @@
+function fizzBuzz(n) {
+    let answer = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            answer.push('FizzBuzz');
+        } else if (i % 3 === 0) {
+            answer.push('Fizz');
+        } else if (i % 5 === 0) {
+            answer.push('Buzz'); 
+        } else {
+            answer.push(i.toString());
+        }
+    }
+    return answer;
+};
+
+fizzBuzz(3);
+console.log('fizzBuzz:', fizzBuzz(3));
+
+fizzBuzz(5);
+console.log('fizzBuzz', fizzBuzz(5));
