bd92365dd967811def78af8921ba09e2f8b916c7
Kaitlin Lovrich <kaylovrich@gmail.com>
2025-03-05 16:54:07 -0800

    Optimized code

--- 141.linked-list-cycle.js
+++ 141.linked-list-cycle.js
@@ -23,7 +23,7 @@ var hasCycle = function (head) {
     while (fast && fast.next) {
         slow = slow.next;
         fast = fast.next.next;
-        if (fast.next.next === slow.next) {
+        if (fast === slow) {
             return true;
         }
     }
